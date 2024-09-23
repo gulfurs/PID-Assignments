7886190723ade3f0aa0ff3b3bf5c44ef3f246d1b
Simon <gulfurs@gmail.com>
2024-09-23 12:17:56 +0200

    pwease work

---
+++ VRDEV_part1/Assets/Scripts/HandAniController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public InputActionProperty PinchAnimation;
+    public InputActionProperty GrabnAnimation;
+
+    public Animator HandAnimator;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float pinchValue = PinchAnimation.action.ReadValue<float>();
+        HandAnimator.SetFloat("Pinch", pinchValue);
+
+        float grabValue = GrabnAnimation.action.ReadValue<float>();
+        HandAnimator.SetFloat("Grab", grabValue);
+    }
+}
